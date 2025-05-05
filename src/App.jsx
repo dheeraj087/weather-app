75fe0e787334015eceadb3078f4688be387c190f
dheeraj1231 <dwivedidheeraj087@gmail.com>
2025-05-05 16:02:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -146,6 +146,8 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/dheeraj087/weather.git
+git remote add origin https://github.com/dheeraj087/weather-app.git
+git remote set-url origin https://github.com/dheeraj087/weather-app.git
+
 git push -u origin main
 */
